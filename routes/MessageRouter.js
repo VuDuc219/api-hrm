b9446a87e6c31cf326959337e8c2fb370d52b01e
KhacAnh123 <tamthanhtinhclq@gmail.com>
2024-07-01 10:04:30 +0700

    deploy

--- routes/MessageRouter.js
+++ routes/MessageRouter.js
@@ -1,15 +1,15 @@
-// const express = require('express');
-// const MessageController = require('../controllers/messageController');
-// const { authMiddleware } = require('../middleware/authMiddleware');
+// // const express = require('express');
+// // const MessageController = require('../controllers/messageController');
+// // const { authMiddleware } = require('../middleware/authMiddleware');
 
-import express from 'express';
-import MessageController from '../controllers/messageController.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
+// import express from 'express';
+// import MessageController from '../controllers/messageController.js';
+// import { authMiddleware } from '../middleware/authMiddleware.js';
 
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/create-message', MessageController.createMessage);
-router.get('/get-messages/:conversationId', MessageController.getMessages);
+// router.post('/create-message', MessageController.createMessage);
+// router.get('/get-messages/:conversationId', MessageController.getMessages);
 
-export default router;
+// export default router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import UserRouter from './UserRouter.js';
 import RefreshTokenRouter from './RefreshTokenRouter.js';
 import ConversationRouter from './ConversationRouter.js';
-import MessageRouter from './MessageRouter.js';
+// import MessageRouter from './MessageRouter.js';
 import NotificationRouter from './NotificationRouter.js';
 import PostRouter from './PostRouter.js';
 import CommentRouter from './CommentRoute.js';
@@ -12,7 +12,7 @@ const routes = (app) => {
     // app.use('/api/v1/user', UserRouter)
     app.use('/api/v1/user-token', RefreshTokenRouter)
     app.use('/api/v1/conversation', ConversationRouter)
-    app.use('/api/v1/message', MessageRouter)
+    // app.use('/api/v1/message', MessageRouter)
     app.use('/api/v1/notification', NotificationRouter)
     app.use('/api/v1/post', PostRouter)
     app.use('/api/v1/comment', CommentRouter)
